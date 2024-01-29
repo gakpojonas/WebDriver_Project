0efcbd031acc7b3de9b243656f7e33e652ef39b4
jonas.gakpo <jonas.gakpo@tech11.com>
2024-01-29 18:21:42 +0000

    First push

---
+++ test/specs/mytest.e2e.js
@@ -0,0 +1,24 @@
+
+
+describe('Demo Tests', function () {
+
+    it('My First Test', async () => {
+
+       browser.url('https://qatest-dev.indvp.com/');
+
+       const mySearchField = $(".Header-Button_type_account");
+
+       await mySearchField.click();
+
+       await $("#email").setValue("jonasgakpo14@gmail.com");
+
+       await $("#password").setValue("Cantona12");
+
+       await browser.pause(4000);
+
+       await $$(".Button")[2].click();
+
+       await browser.pause(4000);
+    
+    })
+})
